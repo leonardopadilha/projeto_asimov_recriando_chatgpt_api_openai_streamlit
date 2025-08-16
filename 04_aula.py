1f39a5696bbd19d609b6145c1194eee596ca64c7
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-16 06:59:05 -0300

    Salvando as conversas em arquivos

--- 04_aula.py
+++ 04_aula.py
@@ -1,11 +1,18 @@
 import os
+import re
+import pickle
 import openai
 import streamlit as st
+from pathlib import Path
 from dotenv import load_dotenv
+from unidecode import unidecode
 
 load_dotenv()
 
 openai_key = os.getenv("OPENAI_API_KEY")
+
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
 
 MODELO = "gpt-3.5-turbo"
 
@@ -19,11 +26,43 @@ def retorna_resposta_modelo(mensagens,openai_key, modelo=MODELO, temperatura=0, 
   )
   return response
 
+def converte_nome_mensagem(nome_mensagem):
+  nome_arquivo = unidecode(nome_mensagem) # Removendo acentos
+  nome_arquivo = re.sub('\W+', '', nome_arquivo).lower() # Removendo caracteres especiais
+  return nome_arquivo
+
+def retorna_nome_da_mensagem(mensagens):
+  nome_mensagem = ''
+  for mensagem in mensagens:
+    if mensagem['role'] == 'user':
+      nome_mensagem  = mensagem['content'][:30]
+      break
+  return nome_mensagem
+
+def salvar_mensagens(mensagens):
+  if len(mensagens) == 0:
+    return False
+  nome_mensagem = retorna_nome_da_mensagem(mensagens)
+  nome_arquivo = converte_nome_mensagem(nome_mensagem)
+  arquivo_salvar = { 'nome_mensagem': nome_mensagem, 'nome_arquivo': nome_arquivo, 'mensagem': mensagens }
+
+  with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
+    pickle.dump(arquivo_salvar, f) # Salva o arquivo
+
+def ler_mensagens(mensagens, key='mensagem'):
+  if len(mensagens) == 0:
+    return []
+  nome_mensagem = retorna_nome_da_mensagem(mensagens)
+  nome_arquivo = converte_nome_mensagem(nome_mensagem)
+  with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+    mensagens = pickle.load(f)
+  return mensagens[key]
+
 def pagina_principal():
   if not 'mensagens' in st.session_state: # session_state é a memória do streamlit
     st.session_state.mensagens = []
 
-  mensagens = st.session_state["mensagens"]
+  mensagens = ler_mensagens(st.session_state['mensagens'])
 
   st.header("🤖 AsimovGPT", divider=True)
 
@@ -52,6 +91,7 @@ def pagina_principal():
     mensagens.append(nova_mensagem)
 
     st.session_state['mensagens'] = mensagens
+    salvar_mensagens(mensagens)
 
 
 pagina_principal()
